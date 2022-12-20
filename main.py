8186e914f1d82ff1c1eb6fc49dfcc954d590710c
Aika1103 <aiganish88@mail.ru>
2022-12-20 12:28:29 +0600

    .

---
+++ main.py
@@ -0,0 +1,51 @@
+import telebot
+import random
+from env import TOKEN
+
+
+
+
+bot = telebot.TeleBot(TOKEN)
+
+keyboard = telebot.types.ReplyKeyboardMarkup()
+button1 = telebot.types.KeyboardButton('Да')
+button2 = telebot.types.KeyboardButton('Нет')
+keyboard.add(button1, button2)
+
+
+@bot.message_handler(commands=['start', 'hi'])
+def start_function(message):
+    msg = bot.send_message(message.chat.id,f'Привет!{message.chat.first_name} начнем игру?',reply_markup=keyboard)
+    bot.register_next_step_handler(message, answer_check)
+#     bot.send_sticker(message.chat.id,'CAACAgIAAxkBAAJKgWOhPi0GVzYQO5GKu26m4MxWCvH-AAL7DAAC0z9hS-vkFkvmPoUiLAQ')
+#     bot.send_photo(message.chat.id,'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png' )
+ 
+def answer_check(msg):
+    if msg.text == 'Да':
+        bot.send_message(msg.chat.id, 'У тебя есть 3 попытки угадать число от 1 до 10')
+        random_number = random.randint(1,10)
+        p = 3
+        start_game(msg, random_number, p-1)
+
+    else:
+        bot.send_message(msg.chat.id, 'Ну и ладно!')
+
+def start_game(msg, random_number, p):
+    msg = bot.send_message(msg.chat.id, 'Ввведи число от 1 до 10: ')  
+    bot.register_next_step_handler(msg, check_func, random_number, p-1)     
+
+def check_func(msg, random_number, p):
+    if msg.text == str(random_number):
+        bot.send_message(msg.chat.id, 'Вы победили!')
+    elif p == 0:
+        bot.send_message(msg.chat.id, f'Вы проиграли! Число было - {random_number}')
+    else:
+        bot.send_message(msg.chat.id, f'Попробуй еще раз, у тебя осталась {p} попыток')
+        start_game(msg, random_number, p)
+
+# @bot.message_handler()
+# def echo_all(message):
+#     bot.send_message(message.chat.id, message.text) 
+    # print(msg.text)  
+
+bot.polling()
